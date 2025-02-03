0d9988908b9491293ea2b017f4c3d37a02371d85
Luke Olende <olendeluke2k@gmail.com>
2025-02-03 11:49:40 +0300

    Add snake and food to the canvas

--- snake.js
+++ snake.js
@@ -1,9 +1,17 @@
-// board
+// Board
 var blockSize = 25;
 var rows = 20;
 var cols = 20;
 var board;
 var context;
+
+// Snake Head
+snakeX = blockSize * 5;
+snakeY = blockSize * 5;
+
+// Foood
+var foodX = blockSize * 10;
+var foodY = blockSize * 10;
 
 window.onload = () => {
   board = document.getElementById("board");
@@ -17,4 +25,10 @@ window.onload = () => {
 function update() {
   context.fillStyle = "black";
   context.fillRect(0, 0, board.width, board.height);
+
+  context.fillStyle = "lime";
+  context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+  context.fillStyle = "red";
+  context.fillRect(foodX, foodY, blockSize, blockSize);
 }
